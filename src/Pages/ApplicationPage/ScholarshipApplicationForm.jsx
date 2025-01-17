b3563f2b5a6db5efc2d2093249c0bc637da539e3
Chanbadsha <chanbadsha005@gmail.com>
2025-01-18 02:00:57 +0600

    responsive application form

--- src/Pages/ApplicationPage/ScholarshipApplicationForm.jsx
+++ src/Pages/ApplicationPage/ScholarshipApplicationForm.jsx
@@ -8,7 +8,7 @@ import useAxiosSecret from "../../Hooks/Axios/AxiosSecret/useAxiosSecret";
 import toast from "react-hot-toast";
 
 const ScholarshipApplicationForm = () => {
-  const { loading, user } = useAuth();
+  const { loading, user, isDarkMode } = useAuth();
   const { id } = useParams();
   if (loading) {
     return <Loader />;
@@ -19,16 +19,19 @@ const ScholarshipApplicationForm = () => {
   const axiosSecret = useAxiosSecret();
   const [scholarship, setScholarship] = useState([]);
   const [userInfo, setUserInfo] = useState(null);
+
   useEffect(() => {
     axiosPublic
       .get(`/users?email=${user.email}`)
       .then((res) => setUserInfo(res.data));
   }, []);
+
   useEffect(() => {
     axiosPublic.get(`/scholarships/${id}`).then((res) => {
       setScholarship(res.data);
     });
   }, [id]);
+
   const {
     register,
     handleSubmit,
@@ -38,12 +41,9 @@ const ScholarshipApplicationForm = () => {
   const onSubmit = async (data) => {
     const { photo, ...applicant_info } = data || {};
 
-    // console.log(photo[0]);
     const imageFile = { image: photo[0] };
-    // console.log(imageFile);
     const date = new Date();
-
-    applicant_info.user_name = userInfo.name;
+    applicant_info.student_name = applicant_info.user_name = userInfo.name;
     applicant_info.user_email = userInfo.email;
     applicant_info.user_id = userInfo._id;
     applicant_info.Scholarship_id = scholarship._id;
@@ -57,7 +57,6 @@ const ScholarshipApplicationForm = () => {
     applicant_info.user_img = res.data.data.display_url;
     if (res.data.success) {
       axiosSecret.post("/application", applicant_info).then((res) => {
-        console.log(res.data.insertedId);
         if (res.data.insertedId) {
           toast.success("Your application has been successfully submitted!");
         }
@@ -66,7 +65,13 @@ const ScholarshipApplicationForm = () => {
   };
 
   return (
-    <div className="bg-white min-h-screen">
+    <div
+      className={`${
+        isDarkMode
+          ? "bg-gray-900 text-textBlack"
+          : "bg-backgroundLight text-textLight"
+      }`}
+    >
       {/* Header Section */}
       <header className="bg-blue-500 text-white py-6 shadow-md">
         <div className="container mx-auto px-4">
@@ -80,12 +85,14 @@ const ScholarshipApplicationForm = () => {
       </header>
 
       {/* Main Content */}
-      <main className="container mx-auto px-4 py-8">
+      <main className={`container mx-auto px-4 py-8`}>
         <form
-          className="max-w-5xl mx-auto p-6 bg-gray-50 rounded-lg shadow-md"
+          className={`max-w-4xl mx-auto p-6 rounded-lg  shadow-2xl ${
+            isDarkMode ? "bg-backgroundBlack" : "bg-gray-100"
+          }`}
           onSubmit={handleSubmit(onSubmit)}
         >
-          <h2 className="text-2xl font-bold mb-6 text-center text-gray-700">
+          <h2 className="text-2xl font-bold mb-6 text-center text-blue-500">
             Application Form
           </h2>
 
@@ -94,24 +101,43 @@ const ScholarshipApplicationForm = () => {
             <label className="block font-semibold mb-1">Applicant's Name</label>
             <input
               type="text"
-              className="w-full px-3 py-2 border rounded bg-gray-200"
-              value={user?.displayName}
-              readOnly
+              className={`w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 ${
+                isDarkMode
+                  ? "bg-backgroundBlack text-textBlack"
+                  : "text-textLight bg-backgroundLight"
+              }`}
+              defaultValue={user?.displayName}
+              {...register("name", { required: "Name is required" })}
             />
+            {errors.name && (
+              <p className="text-red-500 text-sm">
+                {errors.phoneNumber.message}
+              </p>
+            )}
           </div>
 
-          <div className="flex flex-col md:flex-row  md:gap-4">
+          <div className="flex flex-col md:flex-row md:gap-4">
             {/* Phone Number */}
             <div className="mb-4 flex-1">
-              <label className="block font-semibold mb-1">
-                Applicant's Phone Number
-              </label>
+              <label className="block font-semibold mb-1">Phone Number</label>
               <input
                 type="text"
                 placeholder="Enter phone number"
-                className="w-full px-3 py-2 border rounded  focus:ring focus:ring-blue-200"
+                className={`w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 ${
+                  isDarkMode
+                    ? "bg-backgroundBlack text-textBlack"
+                    : "text-textLight bg-backgroundLight"
+                }`}
                 {...register("phoneNumber", {
                   required: "Phone number is required",
+                  minLength: {
+                    value: 10,
+                    message: "Phone number should be at least 10 digits",
+                  },
+                  pattern: {
+                    value: /^[\+\-\d\s]*$/,
+                    message: "Phone number can only contain digits",
+                  },
                 })}
               />
               {errors.phoneNumber && (
@@ -123,11 +149,13 @@ const ScholarshipApplicationForm = () => {
 
             {/* Gender */}
             <div className="mb-4 flex-1">
-              <label className="block font-semibold mb-1">
-                Applicant's Gender
-              </label>
+              <label className="block font-semibold mb-1">Gender</label>
               <select
-                className="w-full px-3 py-2 border rounded focus:ring focus:ring-blue-200"
+                className={`w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 ${
+                  isDarkMode
+                    ? "bg-backgroundBlack text-textBlack"
+                    : "text-textLight bg-backgroundLight"
+                }`}
                 {...register("gender", { required: "Gender is required" })}
               >
                 <option value="">Select Gender</option>
@@ -140,12 +168,13 @@ const ScholarshipApplicationForm = () => {
               )}
             </div>
           </div>
+
           {/* Photo */}
           <div className="mb-4">
             <label className="block font-semibold mb-1">Upload Photo</label>
             <input
               type="file"
-              className="w-full px-3 py-2 border rounded focus:ring focus:ring-blue-200"
+              className="w-full px-3 py-2 border rounded-md focus:ring-2 focus:ring-blue-500"
               {...register("photo", { required: "Photo is required" })}
             />
             {errors.photo && (
@@ -154,14 +183,16 @@ const ScholarshipApplicationForm = () => {
           </div>
 
           {/* Address */}
-          <div className="mb-4">
-            <label className="block font-semibold mb-1">
-              Applicant's Address
-            </label>
+          <div className="mb-4 space-y-2">
+            <label className="block font-semibold mb-1">Address</label>
             <input
               type="text"
               placeholder="Village"
-              className="w-full px-3 py-2 border rounded mb-2 focus:ring focus:ring-blue-200"
+              className={`w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 ${
+                isDarkMode
+                  ? "bg-backgroundBlack text-textBlack"
+                  : "text-textLight bg-backgroundLight"
+              }`}
               {...register("address.village", {
                 required: "Village is required",
               })}
@@ -169,7 +200,11 @@ const ScholarshipApplicationForm = () => {
             <input
               type="text"
               placeholder="District"
-              className="w-full px-3 py-2 border rounded mb-2 focus:ring focus:ring-blue-200"
+              className={`w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 ${
+                isDarkMode
+                  ? "bg-backgroundBlack text-textBlack"
+                  : "text-textLight bg-backgroundLight"
+              }`}
               {...register("address.district", {
                 required: "District is required",
               })}
@@ -177,7 +212,11 @@ const ScholarshipApplicationForm = () => {
             <input
               type="text"
               placeholder="Country"
-              className="w-full px-3 py-2 border rounded focus:ring focus:ring-blue-200"
+              className={`w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 ${
+                isDarkMode
+                  ? "bg-backgroundBlack text-textBlack"
+                  : "text-textLight bg-backgroundLight"
+              }`}
               {...register("address.country", {
                 required: "Country is required",
               })}
@@ -189,15 +228,18 @@ const ScholarshipApplicationForm = () => {
             )}
           </div>
 
-          {/* applying degree */}
-          <div className="flex flex-col md:flex-row  md:gap-4">
-            {/* Applying Degree */}
+          {/* Degree and Study Gap */}
+          <div className="flex flex-col md:flex-row md:gap-4">
             <div className="mb-4 flex-1">
               <label className="block font-semibold mb-1">
                 Applying Degree
               </label>
               <select
-                className="w-full px-3 py-2 border rounded focus:ring focus:ring-blue-200"
+                className={`w-full  px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 ${
+                  isDarkMode
+                    ? "bg-backgroundBlack text-textBlack"
+                    : "text-textLight bg-backgroundLight"
+                }`}
                 {...register("degree", { required: "Degree is required" })}
               >
                 <option value="">Select Degree</option>
@@ -209,11 +251,14 @@ const ScholarshipApplicationForm = () => {
                 <p className="text-red-500 text-sm">{errors.degree.message}</p>
               )}
             </div>
-            {/* Study Gap */}
             <div className="mb-4 flex-1">
               <label className="block font-semibold mb-1">Study Gap</label>
               <select
-                className="w-full px-3 py-2 border rounded focus:ring focus:ring-blue-200"
+                className={`w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 ${
+                  isDarkMode
+                    ? "bg-backgroundBlack text-textBlack"
+                    : "text-textLight bg-backgroundLight"
+                }`}
                 {...register("studyGap")}
               >
                 <option value="">No Gap</option>
@@ -224,17 +269,29 @@ const ScholarshipApplicationForm = () => {
             </div>
           </div>
 
-          {/* Result */}
-          <div className="flex flex-col md:flex-row  md:gap-4">
-            {/* SSC Result */}
+          {/* SSC and HSC Result */}
+          <div className="flex flex-col md:flex-row md:gap-4">
             <div className="mb-4 flex-1">
               <label className="block font-semibold mb-1">SSC Result</label>
               <input
-                type="text"
+                type="number"
+                step="0.1"
                 placeholder="Enter SSC result"
-                className="w-full px-3 py-2 border rounded focus:ring focus:ring-blue-200"
+                className={`w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 ${
+                  isDarkMode
+                    ? "bg-backgroundBlack text-textBlack"
+                    : "text-textLight bg-backgroundLight"
+                }`}
                 {...register("sscResult", {
                   required: "SSC Result is required",
+                  min: {
+                    value: 0,
+                    message: "SSC Result cannot be less than 0",
+                  },
+                  max: {
+                    value: 5,
+                    message: "SSC Result cannot be greater than 5",
+                  },
                 })}
               />
               {errors.sscResult && (
@@ -244,17 +301,30 @@ const ScholarshipApplicationForm = () => {
               )}
             </div>
 
-            {/* HSC Result */}
             <div className="mb-4 flex-1">
               <label className="block font-semibold mb-1">HSC Result</label>
               <input
-                type="text"
+                type="number"
+                step="0.1"
                 placeholder="Enter HSC result"
-                className="w-full px-3 py-2 border rounded focus:ring focus:ring-blue-200"
+                className={`w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 ${
+                  isDarkMode
+                    ? "bg-backgroundBlack text-textBlack"
+                    : "text-textLight bg-backgroundLight"
+                }`}
                 {...register("hscResult", {
                   required: "HSC Result is required",
+                  min: {
+                    value: 0,
+                    message: "HSC Result cannot be less than 0",
+                  },
+                  max: {
+                    value: 5,
+                    message: "HSC Result cannot be greater than 5",
+                  },
                 })}
               />
+
               {errors.hscResult && (
                 <p className="text-red-500 text-sm">
                   {errors.hscResult.message}
@@ -264,24 +334,20 @@ const ScholarshipApplicationForm = () => {
           </div>
 
           {/* Read-Only Fields */}
-          <div className="mb-4">
-            <label className="block font-semibold mb-1">University Name</label>
-            <input
-              type="text"
-              className="w-full px-3 py-2 border rounded bg-gray-200"
-              value={`${scholarship.university_name}`}
-              readOnly
-            />
-          </div>
-          <div className="flex flex-col md:flex-row  md:gap-4">
+          <div className="flex flex-col md:flex-row md:gap-4">
+            {" "}
             <div className="mb-4 flex-1">
               <label className="block font-semibold mb-1">
-                Scholarship Category
+                University Name
               </label>
               <input
                 type="text"
-                className="w-full px-3 py-2 border rounded bg-gray-200"
-                value="Full Scholarship"
+                className={`w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 ${
+                  isDarkMode
+                    ? "bg-backgroundBlack text-textBlack"
+                    : "text-textLight bg-backgroundLight"
+                }`}
+                value={`${scholarship.university_name}`}
                 readOnly
               />
             </div>
@@ -291,19 +357,58 @@ const ScholarshipApplicationForm = () => {
               </label>
               <input
                 type="text"
-                className="w-full px-3 py-2 border rounded bg-gray-200"
+                className={`w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 ${
+                  isDarkMode
+                    ? "bg-backgroundBlack text-textBlack"
+                    : "text-textLight bg-backgroundLight"
+                }`}
                 value={`${scholarship.subject_name}`}
                 readOnly
               />
             </div>
           </div>
+
+          <div className="flex flex-col md:flex-row md:gap-4">
+            <div className="mb-4 flex-1">
+              <label className="block font-semibold mb-1">
+                Scholarship Category
+              </label>
+              <input
+                type="text"
+                className={`w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 ${
+                  isDarkMode
+                    ? "bg-backgroundBlack text-textBlack"
+                    : "text-textLight bg-backgroundLight"
+                }`}
+                value="Full Scholarship"
+                readOnly
+              />
+            </div>
+
+            <div className="mb-4 flex-1">
+              <label className="block font-semibold mb-1">Eligibility</label>
+              <input
+                type="text"
+                className={`w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 ${
+                  isDarkMode
+                    ? "bg-backgroundBlack text-textBlack"
+                    : "text-textLight bg-backgroundLight"
+                }`}
+                value="Eligible students with excellent academic records"
+                readOnly
+              />
+            </div>
+          </div>
+
           {/* Submit Button */}
-          <button
-            type="submit"
-            className="w-full py-2 px-4 bg-blue-500 text-white font-semibold rounded hover:bg-blue-600 focus:ring focus:ring-blue-200"
-          >
-            Submit Application
-          </button>
+          <div className="text-center">
+            <button
+              type="submit"
+              className="bg-blue-500 text-white py-3 px-8 rounded-full font-semibold shadow-lg transform transition-all duration-300 hover:bg-blue-600"
+            >
+              Submit Application
+            </button>
+          </div>
         </form>
       </main>
     </div>
